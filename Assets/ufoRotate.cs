8bcdd2496cc0500dd5d9c11e20650baa48ac0447
swayam1998 <swayam1998@gmail.com>
2020-11-02 13:48:57 +0530

    new spaceship

---
+++ Assets/ufoRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ufoRotate : MonoBehaviour
+{
+    public GameObject target;
+    public float rotSpeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(target.transform.position, Vector3.up, rotSpeed * Time.deltaTime);
+    }
+}
