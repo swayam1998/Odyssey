7dfb4a2802aa63aa872fc4b944cfd428eedaa81e
swayam1998 <swayam1998@gmail.com>
2020-11-05 11:34:35 +0530

    added tractor beam push

--- Assets/cubeController.cs
+++ Assets/cubeController.cs
@@ -8,11 +8,8 @@ public class cubeController : MonoBehaviour
     //public float fuelAdd;
     public GameObject target;
     //private GameObject fuelBar;
-
-    private int tgr = 0;
     public float thrust = 1.0f;
     public Rigidbody rb;
-
     public float timer;
 
     // Start is called before the first frame update
@@ -21,43 +18,12 @@ public class cubeController : MonoBehaviour
         //fuelBar = GameObject.FindGameObjectWithTag("Fuel Bar");
         target = GameObject.FindGameObjectWithTag("Player");
         rb = GetComponent<Rigidbody>();
-        timer = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonUp("Jump"))
-        {
-            tgr = 2;
-            rb.AddForce(transform.forward * thrust);
-        }
-
-        if(tgr == 1)
-        {
-            SetTransformXZ(target.transform.position.x, target.transform.position.z);
-            transform.position += new Vector3(0, suckSpeed * Time.deltaTime, 0);
-
-            if(transform.position.y >= target.transform.position.y)
-            {
-                Destroy(gameObject);
-            }
-        }
-
-        if(tgr == 2)
-        {
-            if(timer <= 10)
-            {
-                rb.AddForce(transform.forward * thrust);
-                timer += Time.deltaTime;
-            }
-
-            else
-            {
-                tgr = 0;
-                timer = 0;
-            }           
-        }
+        
     }
 
     private void OnTriggerEnter(Collider other)
@@ -69,27 +35,33 @@ public class cubeController : MonoBehaviour
 
         if (other.name == "tractorBeam")
         {
-            //StartCoroutine(beamUp());
-            tgr =1;
+            StartCoroutine(beamUp());
+            // tgr = 1;
         }
     }
 
-    // IEnumerator beamUp()
-    // {
-    //     transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
-    //     tgr = 1;
-    //     transform.position = Vector3.MoveTowards(transform.position, transform.position + new Vector3(0, 10, 0), 1);
-        
-    //     // if(transform.position.y >= target.transform.position.y)
-    //     // {
-    //     //     //Destroy(gameObject);
-    //     //     yield return 0;
-    //     // }
-    //     yield return 0;
-    // }
-
-    void SetTransformXZ(float p, float q)
+    private int a=0;
+    IEnumerator beamUp()
     {
-        transform.position = new Vector3(p, transform.position.y, q);
+        while(transform.position.y < target.transform.position.y)
+        {
+            transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+            transform.position += new Vector3(0, suckSpeed * Time.deltaTime, 0);
+
+            if (Input.GetButtonUp("Jump"))
+            {
+                rb.velocity = transform.forward * thrust;
+                break;
+            }                
+
+            yield return null;
+        }
+
+        if (transform.position.y >= target.transform.position.y)
+        {
+            Destroy(gameObject);
+            yield return null;
+        }
     }
+
 }
